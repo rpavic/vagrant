65e6cc2e53ca0aa79efaac9e748b308d2684203d
Ranko Pavić <rpavic@gmail.com>
2020-02-12 09:42:27 +0100

    Add IIS machine

---
+++ scripts/DeployIIS.ps1
@@ -0,0 +1,3 @@
+# Install IIS
+Write-Host "Installing IIS"
+Add-WindowsFeature -Name "Web-Server", "Web-WebServer", "Web-Common-Http", "Web-Default-Doc", "Web-Dir-Browsing", "Web-Http-Errors", "Web-Static-Content", "Web-Health", "Web-Http-Logging", "Web-Performance", "Web-Stat-Compression", "Web-Security", "Web-Filtering", "Web-Windows-Auth", "Web-App-Dev", "Web-Net-Ext45", "Web-Asp-Net45", "Web-ISAPI-Ext", "Web-ISAPI-Filter", "Web-Mgmt-Tools", "Web-Mgmt-Console", "Web-Mgmt-Compat", "Web-Metabase", "NET-Framework-45-Features", "NET-Framework-45-Core", "NET-Framework-45-ASPNET", "NET-WCF-Services45", "NET-WCF-TCP-Activation45", "NET-WCF-TCP-PortSharing45" -IncludeManagementTools -LogPath "C:\Temp\WebServerInstall.log"
